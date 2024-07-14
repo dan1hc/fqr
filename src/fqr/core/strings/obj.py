0ec6bfa7eba03ab40fb4799e2a8b2cbfcfe87720
dan1hc <dan@1howardcapital.com>
2024-07-14 16:22:12 -0700

    perf: __regex_constraints__

--- src/fqr/core/strings/obj.py
+++ src/fqr/core/strings/obj.py
@@ -23,11 +23,11 @@ class Pattern:
     """
 
     CamelToSnakeReplacements = lib.re.compile(
-        r'[A-Z0-9]([0-9]{1,128}|[a-z]+|([0-9][a-z]){1,128})'
+        r'[A-Z0-9]([0-9]+|[a-z]+|([0-9][a-z])+)'
         )
     """Matches all Title Case and numeric components."""
 
-    camelCase = lib.re.compile(r'^[a-z]+((\d)|([A-Z0-9][a-z0-9]{1,128})){0,128}$')
+    camelCase = lib.re.compile(r'^[a-z]+((\d)|([A-Z0-9][a-z0-9]{1,128})){0,32}$')
     """
     Matches strict [lower] camelCase (i.e. RESTful casing) according to \
     the [Google Java Style Guide](https://google.github.io/styleguide/javaguide.html#s5.3-camel-case).
@@ -37,19 +37,31 @@ class Pattern:
     Unlike Google, does NOT allow for an optional uppercase character at \
     the end of the string.
 
+    Strings with moreThan32IndividualWords or \
+    withWordsLongerThan128Characters will not be parsed.
+
     """
 
-    snake_case = lib.re.compile(r'^[a-z0-9_]{1,128}$')
-    """Matches strict [lower] snake_case (i.e. python casing)."""
+    snake_case = lib.re.compile(r'^[a-z0-9_]{1,256}$')
+    """
+    Matches strict [lower] snake_case (i.e. python casing).
+
+    ---
+
+    Strings longer than 256 characters will not be matched.
+
+    """
 
     NumberPattern = lib.re.compile(
-        r'^(([+-]?(([0-9]{1,128}(_?[0-9]{1,128}){0,128})?(\.([0-9]{1,128}(_?[0-9]{1,128}){0,128})?)?))+(e[+-]?([0-9]{1,128}(_?[0-9]{1,128}){0,128}){1,128})?)'  # noqa
-        r'(j|(([+-](([0-9]{1,128}(_?[0-9]{1,128}){0,128})?(\.([0-9]{1,128}(_?[0-9]{1,128}){0,128})?)?))+(e[+-]?([0-9]{1,128}(_?[0-9]{1,128}){0,128}){1,128})?j))?$'  # noqa
+        r'^(([+-]?(([0-9](_?[0-9]){0,64})?(\.([0-9](_?[0-9]){0,64})?)?))+(e[+-]?([0-9](_?[0-9]){0,64})+)?)'  # noqa
+        r'(j|(([+-](([0-9](_?[0-9]){0,64})?(\.([0-9](_?[0-9]){0,64})?)?))+(e[+-]?([0-9](_?[0-9]){0,64})+)?j))?$'  # noqa
         )
     """
     Matches integers, floats, scientific notation, and complex numbers.
 
     ---
+
+    Supports precision up to 64 digits either side of a decimal point.
 
     Recognizes valid, pythonic underscore usage as well.
 
